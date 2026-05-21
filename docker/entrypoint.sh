403b4c1d1b99364a52e4d1abdd1a92bc0d6e34ed
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2026-05-21 21:48:48 +0200

    ci without migrations

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+#php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 php bin/console cache:warmup --env=prod
 
 exec supervisord -c /etc/supervisord.conf
