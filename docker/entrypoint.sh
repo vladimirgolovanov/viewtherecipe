a9e034a495ad75233825ecf3d50511aaf79101e6
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2026-05-25 15:45:54 +0200

    ci

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 php bin/console cache:warmup --env=prod
+chown -R www-data:www-data var/
 
 exec supervisord -c /etc/supervisord.conf
