a3619140467032ed21a47153d26810694896752c
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2026-06-17 17:03:38 +0200

    OAuth keys ownership

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 php bin/console cache:warmup --env=prod
-chown -R www-data:www-data var/
+chown -R www-data:www-data var/cache var/log var/share
 
 exec supervisord -c /etc/supervisord.conf
