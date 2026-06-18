2dd3478a127e8f8ccf1942ba9ff8d8d52d99b7e6
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2026-06-18 13:35:58 +0200

    Client repo intf

--- src/Repository/OAuth2ClientRepository.php
+++ src/Repository/OAuth2ClientRepository.php
@@ -8,15 +8,36 @@ use App\Entity\OAuth2Client;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
 
 /**
  * @extends ServiceEntityRepository<OAuth2Client>
  */
-class OAuth2ClientRepository extends ServiceEntityRepository
+class OAuth2ClientRepository extends ServiceEntityRepository implements ClientRepositoryInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, OAuth2Client::class);
+    }
+
+    public function getClientEntity(string $clientIdentifier): ?OAuth2Client
+    {
+        return $this->findOneBy(['identifier' => $clientIdentifier]);
+    }
+
+    public function validateClient(string $clientIdentifier, ?string $clientSecret, ?string $grantType): bool
+    {
+        $client = $this->findOneBy(['identifier' => $clientIdentifier]);
+
+        if (null === $client) {
+            return false;
+        }
+
+        if ($client->isConfidential() && !$client->validateSecret((string) $clientSecret)) {
+            return false;
+        }
+
+        return true;
     }
 
     public function findByUser(User $user): ?OAuth2Client
