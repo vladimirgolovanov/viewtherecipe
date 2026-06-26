31b1689e6883b9f0a38d4aff7c55dd7cb06e7774
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2026-06-26 22:17:34 +0200

    Log all requests

---
+++ src/EventListener/RequestLoggerListener.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+final class RequestLoggerListener
+{
+    public function __construct(
+        private readonly LoggerInterface $logger
+    ) {
+    }
+
+    #[AsEventListener]
+    public function onRequestEvent(RequestEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        $this->logger->info('Incoming request', [
+            'method' => $request->getMethod(),
+            'uri' => $request->getUri(),
+            'query' => $request->query->all(),
+            'body' => $request->getContent(),
+            'headers' => $request->headers->all(),
+            'client_ip' => $request->getClientIp(),
+        ]);
+    }
+
+    #[AsEventListener]
+    public function onResponseEvent(ResponseEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+
+        $this->logger->info('Outgoing response', [
+            'status' => $response->getStatusCode(),
+            'body'   => $response->getContent(),
+        ]);
+    }
+}
