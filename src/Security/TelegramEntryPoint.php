95e6da18ae4d78c3bc19f2b74d8815800d023ec0
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2026-06-29 12:35:33 +0200

    Authorize require login

--- src/Security/TelegramEntryPoint.php
+++ src/Security/TelegramEntryPoint.php
@@ -3,6 +3,7 @@
 namespace App\Security;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -12,6 +13,12 @@ class TelegramEntryPoint implements AuthenticationEntryPointInterface
 {
     public function start(Request $request, ?AuthenticationException $authException = null): Response
     {
+        if ($request->getPreferredFormat() === 'html'
+            || str_contains($request->headers->get('Accept', ''), 'text/html')
+        ) {
+            return new RedirectResponse('/login?redirect='.urlencode($request->getUri()));
+        }
+
         return new JsonResponse(['error' => 'Authentication required.'], Response::HTTP_UNAUTHORIZED);
     }
 }
